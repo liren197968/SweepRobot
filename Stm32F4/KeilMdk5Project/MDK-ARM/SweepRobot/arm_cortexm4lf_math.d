84c9c2b2e21d8fc89fdfa6f5c8e5eeb66218d53e
liren197968 <1979379768@qq.com>
2019-05-12 00:51:19 +0800

    ENH: DOC: delete serval build files

--- Stm32F4/KeilMdk5Project/MDK-ARM/SweepRobot/arm_cortexm4lf_math.d
+++
@@ -1,1 +0,0 @@
-sweeprobot\arm_cortexm4lf_math.o: ..\Drivers\CMSIS\Lib\ARM\arm_cortexM4lf_math.lib
--- Stm32F4/KeilMdk5Project/MDK-ARM/SweepRobot/zmod44xx.d
+++
@@ -1,5 +0,0 @@
-sweeprobot\zmod44xx.o: ..\Zmod4410\Src\zmod44xx.c
-sweeprobot\zmod44xx.o: ..\Zmod4410\Inc\zmod44xx.h
-sweeprobot\zmod44xx.o: ..\Zmod4410\Inc\zmod44xx_config.h
-sweeprobot\zmod44xx.o: D:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-sweeprobot\zmod44xx.o: ..\Zmod4410\Inc\zmod44xx_types.h
